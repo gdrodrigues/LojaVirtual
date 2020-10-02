6ddab40331a46bc3492b45e288569f0b8d5b1854
Gabriel <gabrieldr@id.uff.br>
2020-10-02 20:12:57 -0300

    criando projeto

---
+++ LojaVirtual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LojaVirtual
+//
+//  Created by Gabriel Duarte on 02/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
